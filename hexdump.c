b6a9a83c62c893902b7b4edd7d9c42e88921590b
Katie <kmacalintal@middlebury.edu>
2023-02-26 17:10:00 -0500

    created hexdump and hexread files

---
+++ hexdump.c
@@ -0,0 +1,29 @@
+/*
+ * hexdump.c 
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include "util.h"
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	// check if no arguments are provided first 
+	if (argc == 1) {
+		printf("no arguments provided\n");
+	} else {
+
+		// files have been provided 
+		for (i = 1; i < argc; i++) {
+			printf("argument %d: %s", i, argv[i]);	
+			// need to open file
+			// get and save the size of the file 
+			// read contents of the file 
+			// pass contents of file and size the function 
+		}
+	}
+
+}
