4ba3461e129f43c591eb9e48454d2a11a2b796be
Katie <kmacalintal@middlebury.edu>
2023-02-26 22:06:43 -0500

    added relevant headers

--- util.h
+++ util.h
@@ -1,6 +1,11 @@
 #ifndef __UTIL_H
 #define __UTIL_H
 
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <ctype.h>
+
 char *binary_to_hex(void *data, ssize_t n);
 void *hex_to_binary(char *hex);
 
