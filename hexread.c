4dbb9dda203a0659fd8bc75784126f545b522c78
Katie <kmacalintal@middlebury.edu>
2023-02-26 23:27:16 -0500

    added opening and closing of a file

--- hexread.c
+++ hexread.c
@@ -2,27 +2,50 @@
  * hexread.c
  */
 
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
 #include "util.h"
+
+#define BUFF_SIZE 2048
 
 int main(int argc, char *argv[])
 {
 	int i;
+	char input_buffer[BUFF_SIZE];
+	void *results;
+	FILE *file;
 
-	// check if no arguments are provided first 
+	// No files provided
 	if (argc == 1) {
-		printf("no arguments provided\n");
-	} else {
+		printf("No file provided. Please enter the hex you would like to be read:\n");
+		
+		if (fgets(input_buffer, BUFF_SIZE, stdin) == NULL) {
+			printf("An error occurred\n"); // SHOULD I CHANGE THIS? 
+			exit(0);
+		}
 
-		// files have been provided 
+		printf("%s", input_buffer);
+		//results = hex_to_binary(input_buffer);
+		// WHAT DO WE DO WITH THE RESULTS? 
+	
+	// Files provided
+	} else {
 		for (i = 1; i < argc; i++) {
-			printf("argument %d: %s", i, argv[i]);	
-			// need to open file
-			// get and save the size of the file 
-			// read contents of the file 
-			// pass contents of file to function 
+		//	printf("argument %d: %s", i, argv[i]);	
+			
+			if ((file = fopen(argv[i], "r")) == NULL) {
+				perror("fopen");
+			} else {
+
+
+				// read contents of the file 
+				// pass contents of file to function 
+			
+				if (fclose(file) == EOF) {
+					perror("fclose");
+				}
+			}
+			
+			printf("%s:\n", argv[i]);
+			// WHAT DO WE DO WITH THE RESULTS? 
 		}
 	}
 }
