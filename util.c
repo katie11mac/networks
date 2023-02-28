ad2772efbbed46ddc9888faeb873c4d6596e7e95
Katie <kmacalintal@middlebury.edu>
2023-02-28 00:52:06 -0500

    basic hex_to_binary function

--- util.c
+++ util.c
@@ -7,40 +7,49 @@
 int main(int argc, char *argv[]) 
 {
 	char *data = "ABAA"; 
-	int *malloc_int = (int *)malloc(sizeof(int));
+	int *malloc_int = (int *)(malloc(sizeof(int)));
 	char *data_str;
 	char *int_str;
+	void *results;
+//	printf("data: %s\n", data);
+//	printf("%s\n", binary_to_hex(data, 4));
+	data_str = "41   42  41   41 ";
+	printf("converting to binary: %s\n", data_str); 
+	results = hex_to_binary(data_str);
+	printf("%p\n",results);
+	printf("%s\n\n",(char *)results);
 
-	printf("data: %s\n", data);
-	//data_str = binary_to_hex(data, 4);
-	data_str = "41    41  42  41 ";
-//	printf("converting to binary: %s", data_str); 
-	hex_to_binary(data_str);
-
-	data_str = "41   41  42 4";
+//	data_str = "41   41  42 4";
 //	printf("converting to binary: %s", data_str);
-	hex_to_binary(data_str); 
+//	hex_to_binary(data_str); 
 
-	data_str = "Aa KM";
-	hex_to_binary(data_str);
+//	data_str = "Aa KM";
+//	hex_to_binary(data_str);
 
-	malloc_int = 128;
+//	malloc_int = 128;
        	//printf("data: %ls\n", malloc_int); 	
-	//int_str = binary_to_hex(&malloc_int, 1);
-	int_str = "80";
-//	printf("converting to binary: %s", int_str);	
+//	int_str = binary_to_hex(&malloc_int, 4);
+	int_str = "7A";
+	printf("converting to binary:%s\n", int_str);
+	printf("%s\n", (char *)hex_to_binary(int_str));
+	printf("%d\n", *(int *)hex_to_binary(int_str));
 
-	hex_to_binary(int_str);
+	
+//	malloc_int = 300;
+//	int_str = binary_to_hex(&malloc_int, sizeof(int));
+//	printf("%s\n", int_str);
+//	hex_to_binary(int_str);
 
 
 //	data = "ABCDEFGHIJK lmnop QRS tuv WX yz ";
 //	printf("data: %s\n", data);
-//	binary_to_hex(data, 32);
+//	printf("%s\n", binary_to_hex(data, 32));
 	
 
 //	data = "ABCDEFGHIJK lmnop QRS tuv WX yz katie";
 //	printf("data: %s\n", data);
-//	binary_to_hex(data, 37);
+//	printf("%s\n", binary_to_hex(data, 37));
+
 	//binary_to_hex(NULL, 20);
 	
 //	hex_to_binary();
@@ -55,38 +64,38 @@ int main(int argc, char *argv[])
  */
 char *binary_to_hex(void *data, ssize_t n)
 {
-	int new_n, new_lines;
+	int new_n;
 	char *hex_str;
 	int i, char_written;
 	unsigned char *casted_data;
 
-	// Find number of spaces and new lines needed 
-	new_lines = (n / 16) + 1;
-	new_n = (n * 3) + new_lines; 
-	//printf("new_lines: %d\nnew_n: %d\n", new_lines, new_n);
+	// Create new size to include spaces, new lines, and hex digits
+	new_n = n * 3; 
 
 	hex_str = malloc(new_n);
 	
 	// Cast data pointer to dereference it
 	casted_data = (unsigned char *) data;
+	//printf("unsigned char: %u\n", *casted_data );
+	//printf("size of unsigned char: %ld\n", sizeof(*casted_data));
 
 	char_written = 0;
 	for (i = 1; i <= n; i++) {
 		
-		// Index 1 - 1 because initialized i to 1
-		snprintf(hex_str + char_written, new_n,"%02x ", casted_data[i - 1]);
-		char_written += 3;
+		// Index i - 1 because initialized i to 1
+		snprintf(hex_str + char_written, new_n,"%02x", casted_data[i - 1]);
+		char_written += 2;
 		
 		// Add \n every 16th pair
 		if ((i % 16 == 0) & (i != 0)) {
 			snprintf(hex_str + char_written, new_n, "\n");
-			char_written ++;
+		} else {
+			snprintf(hex_str + char_written, new_n, " ");
 		}
+		char_written ++;
 	}
 
 	snprintf(hex_str + char_written, new_n, "\n");
-	//printf("final string: \'%s\'", hex_str); 
-	//printf("\n"); 	
 
 	return hex_str;
 }
@@ -107,8 +116,19 @@ char *binary_to_hex(void *data, ssize_t n)
  */
 void *hex_to_binary(char *hex)
 {
+	ssize_t bytes_expected;
 	int leading_i, trailing_i;
 	char curr_hex[2];
+	void *results;
+	long *curr_addr; 
+	long bytes_used;
+	
+	// Calculate reasonable size for malloc
+	bytes_expected = strlen(hex) / 2;
+	printf("original size: %ld\n", bytes_expected);
+	results = malloc(bytes_expected);
+	curr_addr = (long *)results;
+	
 	trailing_i = 0;
 	leading_i = 0;
 
@@ -129,13 +149,14 @@ void *hex_to_binary(char *hex)
 
 		// Uneven amount of hex digits given 	
 		if (hex[leading_i] == '\0') {
-			printf("(is uneven)\n");
-			return NULL; // CHANGE THIS RETURN STATEMENT
+			//printf("(is uneven)\n");
+			break;
+			//return results; 
 		}
 		
 		// Check characters are valid hex digits 
 		if ((isxdigit(hex[trailing_i]) == 0) | (isxdigit(hex[leading_i]) == 0)) {
-			// NEED TO FREE THE MALLOC-D BUFFER
+			free(results);
 			printf("INVALID HEX DIGIT\n");
 			return NULL;
 		}
@@ -144,14 +165,23 @@ void *hex_to_binary(char *hex)
 		curr_hex[0] = hex[trailing_i];	
 		curr_hex[1] = hex[leading_i];
 
-		printf("%s ",curr_hex);
+		//printf("int: %lx ", strtol(curr_hex, NULL, 16));
+
+		*curr_addr = strtol(curr_hex, NULL, 16);
+		printf("%p: %lx\n", (void *)curr_addr, *curr_addr);
+		curr_addr += 1;
 
 		// Increment indices
 		trailing_i = leading_i + 1;
 		leading_i = trailing_i + 1;;
 	}
 
-	printf("\n"); 
+	// Reallocate if have unused extra space in orginal malloc
+	bytes_used = ((void *)curr_addr - results) / 8;
+	printf("addr gap: %ld\n", bytes_used );
+	if (bytes_used < bytes_expected) {
+		results = realloc(results, bytes_used); 
+	}
 
-	return NULL;
+	return results;
 }
