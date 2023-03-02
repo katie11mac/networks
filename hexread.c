8454b8e643c5f235a61f35af1a01e6c21e01dc17
Katie <kmacalintal@middlebury.edu>
2023-03-01 21:18:46 -0500

    started hexread

--- hexread.c
+++ hexread.c
@@ -4,48 +4,54 @@
 
 #include "util.h"
 
-#define BUFF_SIZE 2048
+#define READ_SIZE 16
+
+void convert_input(FILE *file);
 
 int main(int argc, char *argv[])
 {
 	int i;
-	char input_buffer[BUFF_SIZE];
-	void *results;
 	FILE *file;
 
 	// No files provided
 	if (argc == 1) {
-		printf("No file provided. Please enter the hex you would like to be read:\n");
-		
-		if (fgets(input_buffer, BUFF_SIZE, stdin) == NULL) {
-			printf("An error occurred\n"); // SHOULD I CHANGE THIS? 
-			exit(0);
-		}
-
-		printf("%s", input_buffer);
-		//results = hex_to_binary(input_buffer);
-		// WHAT DO WE DO WITH THE RESULTS? 
+		printf("No file provided. Please enter the hex you would like to be read and enter EOF when you are done:\n");
+		convert_input(stdin);		
 	
 	// Files provided
 	} else {
 		for (i = 1; i < argc; i++) {
-		//	printf("argument %d: %s", i, argv[i]);	
 			
 			if ((file = fopen(argv[i], "r")) == NULL) {
 				perror("fopen");
 			} else {
-
-
-				// read contents of the file 
-				// pass contents of file to function 
-			
+				convert_input(file);	
 				if (fclose(file) == EOF) {
 					perror("fclose");
 				}
 			}
 			
-			printf("%s:\n", argv[i]);
-			// WHAT DO WE DO WITH THE RESULTS? 
 		}
 	}
 }
+
+void convert_input(FILE *file) {
+	char input_buffer[READ_SIZE + 1];
+	void *results;
+	
+	// BUG: memory is never cleared 
+	// BUG: when two hex digits separated by many spaces are sent in separate calls
+	while (fgets(input_buffer, READ_SIZE + 1, file) != NULL) {
+		results = hex_to_binary(input_buffer);
+		if (results == NULL) {
+			// BUG: will convert and print binary until reaches invalid hex digit
+			printf("**PART OF INPUT NOT VALID HEX DIGITS**");
+			break;
+		} else {
+			printf("%s", (char *)results);
+			// should probably memset here, but don't know appropriate size
+			//free(results);
+		}	
+		printf("\n");
+	}
+
