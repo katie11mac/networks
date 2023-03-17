6a3bf74116ad638addd4ad09feb2d11d42136371
Katie <kmacalintal@middlebury.edu>
2023-03-17 16:24:04 -0400

    implemented the array of device structs, started initializing device values with wrong randomly generate wait time, wrote ideas for rest of code

--- ethernet-study/binary_backoff.c
+++ ethernet-study/binary_backoff.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <sys/random.h>
+#include <math.h>
 
 struct device {
 	int has_sent;
@@ -16,12 +17,17 @@ struct device {
 int main(int argc, char *argv[]) {
 
 	int all_devices_sent = 0;
+	uint32_t curr_time = 0;
 	uint32_t num_devices;
-	uint32_t curr_time;
+	struct device *devices;
 
 	uint32_t testing;
+	uint32_t random_num;
 	ssize_t random_result;
+	uint32_t num_bits;
 	uint32_t num;
+
+	int num_devices_sent = 0;
 	
 	if (argc == 2) {
 		num_devices = atoi(argv[1]);
@@ -31,22 +37,65 @@ int main(int argc, char *argv[]) {
 		return 0;
 	}
 
+	// Create n device structs and store in array
+	if ((devices = (struct device *) malloc(num_devices * sizeof(struct device))) == NULL) {
+		printf("malloc failed\n");
+	}
+	
+	// Iterate through devices array and initialize values for devices
+	for (int i = 0; i < num_devices; i++) {
+		devices[i].has_sent = 0;
+		devices[i].num_collisions = 1; // Initalized to 1 bc of our scenario
+		// setting the random value
+		if((random_result = getrandom(&random_num, sizeof(random_num), 0)) == -1) {
+			perror("getrandom");
+			// DO WE WANT TO RETURN? 
+		}
+		// !!!!!! THIS IS WRONG !!!!! need to & with 2 raised to (devices[i].num_collisions) - 1
+		// hmmmm but raising something to the power results in a double and we dont want that 
+		//num_bits = (uint32_t)exp2(devices[i].num_collisions);
+		//		POTENTIAL SOLUTION: change types of num_collisions and random_wait to unsigned int, but then there's ambiguity 
+		devices[i].random_wait = random_num & devices[i].num_collisions;
+	}
+	
+	// lol lets just move on and act like random_wait is correct 
+	
+
+	// Iterate through our array until all devices have sent (while loop)
+	// (can check this either through a boolean variable or 
+	// counting the number of devices that have sent)
+	// 
+	//		- For Loop: Count how many are trying to send (random_time 0)
+	//	****		****	CAN PROBABLY COMBINE THESE TWO LOOPS *****		*****
+	//		- If more than one device is trying to send rn 
+	//			- For Loop through devices looking for those with random_time 0: 
+	//				- increase their number of collisions 
+	//				- have them select a new wait time 
+	//		- If one or none devices are trying to send rn 
+	//			- Floop Loop through devices looking for those with random_time 0: 
+	//				- Mark that device as sent
+	//	****			*****			****			****		*****
+	//		- For Loop: Need to decrement random times 
+	//			- But would we decrement for all devices? Where should this for loop land? 
+	//		- Increament curr_time
+	// ********NEED TO THINK CAREFULLY ABOUT UPDATING VARIABLES************ 
+
+
+	/*
+	// PLAYING AROUND WITH getrandom
 	if((random_result = getrandom(&testing, sizeof(testing), 0)) == -1) {
 		perror("getrandom");
 		// DO WE WANT TO RETURN? 
 	}
 
-	num = 1;
+	//num = 1;
+	//printf("random number: %d\n", testing & num);
+	num = 3;
 	printf("random number: %d\n", testing & num);
-
-
-	// what if we randomly generate the number and store it in random_wait, but only
-	// read the lowest num_collisions bits 
-	//		to read the lowest num_collision bits, would we & it with that number 
-
-	// Malloc n * size of struct to hold an array of devices 
-	// for each i in n, we are gonna want to go to the &array[i] to initalize all the devices 
 	
-	// We need to think more about the generating random numbers to really move on
+	testing = (uint32_t)exp2(1);
+	printf("testing power: %d\n", testing);
+	*/
+	
 
 }
