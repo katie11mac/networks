a7895206d9934c30be253e48a9a160add675ace2
Katie <kmacalintal@middlebury.edu>
2023-03-21 17:54:39 -0400

    ethernet-study: have basic working functionality of sending multiple devices, but the generating random time is still incorrect

--- ethernet-study/binary_backoff.c
+++ ethernet-study/binary_backoff.c
@@ -9,16 +9,19 @@
 #include <math.h>
 
 struct device {
-	int has_sent;
+	int trying_to_send; // should i change this to also represent whether it errored
 	uint32_t num_collisions; // num_collisions - 1 will help us with the range
+	// FIGURE OUT RELATIONSHIP BETWEEN num_collisions and attempts 
 	uint32_t random_wait;
 };
 
 int main(int argc, char *argv[]) {
 
 	int all_devices_sent = 0;
-	uint32_t curr_time = 0;
-	uint32_t num_devices;
+	int num_devices_sent = 0;
+	int curr_time = 0;
+	int num_devices;
+	int num_devices_sending;
 	struct device *devices;
 
 	uint32_t testing;
@@ -27,14 +30,26 @@ int main(int argc, char *argv[]) {
 	uint32_t num_bits;
 	uint32_t num;
 
-	int num_devices_sent = 0;
-	
+	// Read number of devices from command line 
 	if (argc == 2) {
-		num_devices = atoi(argv[1]);
+		
+		if (sscanf(argv[1], "%d", &num_devices) == 0) {
+			printf("Invalid number of devices\n");
+			return 0;
+		}
+		
+		if (num_devices < 1) {
+			printf("Invalid number of devices\n"); 
+			return 0;
+		}
+
 		printf("Starting simulation for %d devices...\n", num_devices);
+	
 	} else {
+		
 		printf("Please provide one command-line argument specifying the number of devices to simulate\n"); 
 		return 0;
+	
 	}
 
 	// Create n device structs and store in array
@@ -44,7 +59,7 @@ int main(int argc, char *argv[]) {
 	
 	// Iterate through devices array and initialize values for devices
 	for (int i = 0; i < num_devices; i++) {
-		devices[i].has_sent = 0;
+		devices[i].trying_to_send = 1;
 		devices[i].num_collisions = 1; // Initalized to 1 bc of our scenario
 		// setting the random value
 		if((random_result = getrandom(&random_num, sizeof(random_num), 0)) == -1) {
@@ -59,7 +74,75 @@ int main(int argc, char *argv[]) {
 	}
 	
 	// lol lets just move on and act like random_wait is correct 
+
+	// Keep looping through all devices until they have all been able to send 
+	while (num_devices_sent < num_devices) {
+
+		// MAKE SURE YOU ARE NOT CHECKING DEVICES THAT HAVE ALREADY SENT
+		//		Note: could technically 
+
+		// Count how many devices are trying to send right now  
+		num_devices_sending = 0;
+		for (int i = 0; i < num_devices; i++) {
+			if ((devices[i].random_wait == -1) && (devices[i].trying_to_send == 1)) {
+				num_devices_sending += 1; 		
+			}
+		}
+		
+
+		// Handle devices that are trying to send on next time slot
+		for (int i = 0; i < num_devices; i++) {
+			
+			// Current device wants to send on next time slot 
+			if ((devices[i].random_wait == -1) && (devices[i].trying_to_send == 1)) {
+				
+				// Only one device is trying to send on next time slot 
+				if (num_devices_sending < 2) {
+					devices[i].trying_to_send = 0;
+					num_devices_sent += 1;
+
+					printf("sending device %d at time %d\n", i, curr_time);
+				
+				// More than one device is trying to send on next time slot
+				} else {
+				
+					printf("COLLISION: device %d trying to send at time %d (collision %d)\n", i, curr_time, devices[i].num_collisions);
+
+					devices[i].num_collisions += 1;
+					
+					// Want to stop trying if has collided more than 10 times
+					if (devices[i].num_collisions > 9) {
+						devices[i].trying_to_send = 0;
+						
+						printf("device %d reached max collisions\n", i);
+						
+						// DON'T KNOW IF num_devices_sent IS GOOD VARIABLE NAME
+						num_devices_sent += 1;
+					}
+
+					// Generate new random wait time 
+					// THIS NEEDS TO GET FIXEDDDD and remember its range increases 
+					if((random_result = getrandom(&random_num, sizeof(random_num), 0)) == -1) {
+						perror("getrandom");
+						// DO WE WANT TO RETURN? 
+					}
+					devices[i].random_wait = random_num & devices[i].num_collisions;
+				
+				}
+			} 
+		}
 	
+		for (int i = 0; i < num_devices; i++) {
+			if (devices[i].trying_to_send == 1) {
+				devices[i].random_wait -= 1;
+
+			}
+		}
+
+		curr_time += 1;	
+
+	}
+		
 
 	// Iterate through our array until all devices have sent (while loop)
 	// (can check this either through a boolean variable or 
@@ -75,8 +158,9 @@ int main(int argc, char *argv[]) {
 	//			- Floop Loop through devices looking for those with random_time 0: 
 	//				- Mark that device as sent
 	//	****			*****			****			****		*****
-	//		- For Loop: Need to decrement random times 
+	//		- For Loop: Need to decrement random times for every single device
 	//			- But would we decrement for all devices? Where should this for loop land? 
+	//		*** WE ALSO NEED TO FIGURE OUT WHEN TO CHECK IF THE DEVICE REACHED MAX COLLISIONS *** 
 	//		- Increament curr_time
 	// ********NEED TO THINK CAREFULLY ABOUT UPDATING VARIABLES************ 
 
