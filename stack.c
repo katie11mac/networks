a128aae6c4c26c3b844f169fb6a4a06eaa99dedf
Katie <kmacalintal@middlebury.edu>
2023-04-08 12:19:49 -0400

    Assignment 3 Part I: Implemented multiple interfaces, modified code to correctly process ethernet frames, reorganized code

--- stack.c
+++ stack.c
@@ -8,28 +8,52 @@ int main(int argc, char *argv[])
 {
     int fds[2];
 
-    uint8_t frame[1600];
-    ssize_t frame_len;
-    char *data_as_hex;
-	
+	// Variables for vde_switch
     int connect_to_remote_switch = 0;
     char *local_vde_cmd[] = { "vde_plug", NULL };
     char *remote_vde_cmd[] = { "ssh", "pjohnson@weathertop.cs.middlebury.edu",
                                       "/home/pjohnson/cs431/bin/vde_plug",
                                       NULL };
     char **vde_cmd = connect_to_remote_switch ? remote_vde_cmd : local_vde_cmd;
-
+	
+	// Variables for frame
+	uint8_t frame[1600];
+    ssize_t frame_len;
+    char *data_as_hex;
+	
+	// Variables for processing recieved frames
 	int is_valid_frame = 1;
-	uint8_t ether_addr[6]; 
 	uint8_t broadcast_addr[6];
 	struct ether_header *curr_frame;
 	ssize_t data_len;
 	uint32_t *fcs_ptr;
 	uint32_t calculated_fcs;
 
+	// Variables for our collection of interfaces 
+	struct interface *interfaces;
+	uint8_t num_interfaces = 4;
 
-	// Create ethernet/MAC address
-	memcpy(ether_addr, "\x86\x46\x6c\x7e\xff\x1a", 6);
+	// ---------- TRIED PUTTING THIS IN A FUNCTION, BUT GOT FUNKY RESULTS ----------
+	if ((interfaces = (struct interface *) malloc(num_interfaces * sizeof(struct interface))) == NULL) {
+		printf("malloc failed\n");
+	}
+	
+	// Interface 0 - WILL BE RECIEVING IN PART I
+	memcpy(interfaces[0].ether_addr, "\x01\x02\x03\x04\xff\xff", 6);
+	memcpy(&interfaces[0].ip_addr, "\x01\02\03\04", 4);
+
+	// Interface 1
+	memcpy(interfaces[1].ether_addr, "\x05\x06\x07\x08\xff\xff", 6);
+	memcpy(&interfaces[1].ip_addr, "\x05\x06\x07\x08", 4);
+
+	// Interface 2
+	memcpy(interfaces[2].ether_addr, "\x09\x0a\x0b\x0c\xff\xff", 6);
+	memcpy(&interfaces[2].ip_addr, "\x09\x0a\x0b\x0c", 4);
+	
+	// Interface 3
+	memcpy(interfaces[3].ether_addr, "\x0d\x0e\x0f\x10\xff\xff", 6);
+	memcpy(&interfaces[3].ip_addr, "\x0d\x0e\x0f\x10", 4);
+	//-----------------------------------------------------------------------------
 
 	// Set broadcast address 
 	memcpy(broadcast_addr, "\xff\xff\xff\xff\xff\xff", 6);
@@ -39,6 +63,7 @@ int main(int argc, char *argv[])
         printf("Could not connect to switch, exiting.\n");
         exit(1);
     }
+
 
 	// Process frames until user terminates with Control-C
     while((frame_len = receive_ethernet_frame(fds[0], frame)) > 0) {
@@ -74,9 +99,12 @@ int main(int argc, char *argv[])
 			}
 		}	
 		
-		// Check if destination is for me
+		// Check if destination is for any of my interfaces
 		if (is_valid_frame) {
-			check_dst_addr(curr_frame, frame_len, broadcast_addr, ether_addr);
+
+			check_dst_addr(curr_frame, frame_len, broadcast_addr, interfaces, num_interfaces); 
+			//printf("jfdksl");
+			//check_dst_addr(curr_frame, frame_len, broadcast_addr, ether_addr);
 		}
 
 		free(data_as_hex);
@@ -89,6 +117,7 @@ int main(int argc, char *argv[])
 
     return 0;
 }
+
 
 /*
  * Return 1 if the frame has a valid length and 0 otherwise 
@@ -109,19 +138,32 @@ int is_valid_frame_length(ssize_t frame_len)
 }
 
 /*
- * Check whether destination address is for me (i.e. broadcast or my MAC address) 
- * and print appropriate messages 
+ * Check whether destination address is for me (i.e. broadcast or interfaces' MAC address) 
+ * and print appropriate messages. 
+ *
+ * Return 0 if frame was a braodcast
+ * Return 1 if frame was for me (i.e. interfaces' MAC address)
+ * Return -1 if frame was not for me 
  */
-void check_dst_addr(struct ether_header *curr_frame, ssize_t frame_len, uint8_t broadcast_addr[6], uint8_t ether_addr[6]) 
+int check_dst_addr(struct ether_header *curr_frame, ssize_t frame_len, uint8_t broadcast_addr[6], struct interface *interfaces, uint8_t num_interfaces) 
 {
 	// Check if frame is a broadcast 
 	if (memcmp(curr_frame->dst, broadcast_addr, 6) == 0) {
 		printf("received %lu-byte broadcast frame from %s", frame_len, binary_to_hex(curr_frame->src, 6)); 
-	// Check if frame is for me 
-	} else if (memcmp(curr_frame->dst, ether_addr, 6) == 0) {
-		printf("received %lu-byte frame for me from %s", frame_len, binary_to_hex(curr_frame->src, 6)); 
-	// Otherwise frame is not for me
-	} else {
-		printf("ignoring %lu-byte frame (not for me)\n", frame_len); 
+		return 1;
 	}
+	
+	for (int i = 0; i < num_interfaces; i++) {
+		// Check if frame is for one of my interfaces 
+		if (memcmp(curr_frame->dst, interfaces[i].ether_addr, 6) == 0) {
+			printf("received %lu-byte frame for me from %s", frame_len, binary_to_hex(curr_frame->src, 6)); 
+			printf("\tframe dest: %s\tinterface MAC: %s", binary_to_hex(curr_frame->dst, 6), binary_to_hex(interfaces[i].ether_addr, 6));
+			// Can return immediately since MAC addresses unique
+			return 1; 
+		}
+	}
+	
+	// Frame is not for any of my interfaces
+	printf("ignoring %lu-byte frame (not for me)\n", frame_len); 
+	return 0;
 }
--- stack.h
+++ stack.h
@@ -19,8 +19,22 @@ struct ether_header {
     uint8_t type[2];
 };
 
+struct ip_address {
+    uint8_t part1; 
+    uint8_t part2;
+    uint8_t part3;
+    uint8_t part4;
+};
+
+struct interface {
+    uint8_t ether_addr[6];
+    struct ip_address ip_addr;  
+};
+
 uint32_t crc32(uint32_t crc, const void *buf, size_t size);
+
+void initialize_interfaces(struct interface *interfaces);
 int is_valid_frame_length(ssize_t frame_len);
-void check_dst_addr(struct ether_header *curr_frame, ssize_t frame_len, uint8_t broadcast_addr[6], uint8_t ether_addr[6]);
+int check_dst_addr(struct ether_header *curr_frame, ssize_t frame_len, uint8_t broadcast_addr[6], struct interface *interfaces, uint8_t num_interfaces);
 
 #endif /* __STACK_H */
