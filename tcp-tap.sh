e61d9c9e7fa9ab4056b10d8907325a4abc32f265
Katie <kmacalintal@middlebury.edu>
2023-05-08 22:50:28 -0400

    edited tcp-tap.sh so that tap0 corresponds with one of my defined networks

--- tcp-tap.sh
+++ tcp-tap.sh
@@ -3,12 +3,12 @@
 # Creates and configures a tap device on FreeBSD along with a vde_switch
 # connected to it.
 
-network="192.168.0.0"
+network="1.2.3.0"
 netmask="255.255.255.0"
 
 vde_switch_ctrl_file="/tmp/net0.vde"
 tap_device="tap0"
-kernel_endpoint_ip="192.168.0.4"
+kernel_endpoint_ip="1.2.3.255"
 
 # Use -hub so Wireshark can see unicast frames.
 vde_switch -sock "$vde_switch_ctrl_file" -hub -daemon
